a52beb03d22dbba7a7c2d4b2b4712491c1c9d157
mrchipmunk <66779513+Mrchipmunk@users.noreply.github.com>
2026-06-14 15:11:46 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+function red () {
+    pins.analogWritePin(AnalogPin.P0, 1023)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 0)
+    return 0
+}
+function Green () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    pins.analogWritePin(AnalogPin.P2, 0)
+    return 0
+}
+function Blue () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 1023)
+    return 0
+}
 basic.forever(function () {
 	
 })
