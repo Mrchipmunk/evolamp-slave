5b45d6372e31aa4408f8a74e08f1a24b706093cc
mrchipmunk <66779513+Mrchipmunk@users.noreply.github.com>
2026-06-14 15:20:31 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+function Off () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 0)
+    return 0
+}
 function red () {
     pins.analogWritePin(AnalogPin.P0, 1023)
     pins.analogWritePin(AnalogPin.P1, 0)
@@ -16,6 +22,13 @@ function Blue () {
     pins.analogWritePin(AnalogPin.P2, 1023)
     return 0
 }
+let Allowed = true
+let Connected = false
 basic.forever(function () {
-	
+    if (Connected == false) {
+        Blue()
+        basic.pause(1000)
+        Off()
+        basic.pause(1000)
+    }
 })
